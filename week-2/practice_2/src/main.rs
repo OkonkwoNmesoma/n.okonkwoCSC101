b3833c0e4a83e9ee9aa24a64e845c2c4214629d4
Nmesoma Okonkwo <nmesoma.okonkwo@pau.edu.ng>
2022-11-11 14:09:38 -0800

    (my second commit)

---
+++ week-2/practice_2/src/main.rs
@@ -0,0 +1,6 @@
+fn main() {
+    println!();  // prinst just a
+    println!("hello"); //prints
+    println!("format {} arguments", "some");  //prints format for some arguments
+    println!("My name is {}. I am the class rep of stream 1","Dagogo William-Jumbo"); 
+}
